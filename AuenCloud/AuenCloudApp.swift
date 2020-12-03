965906e907dbf89e8125fcfc9ad3e413a565f48e
taimanovb <taimanovb@gmail.com>
2020-12-03 22:40:03 +0600

    Initial Commit

---
+++ AuenCloud/AuenCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuenCloudApp.swift
+//  AuenCloud
+//
+//  Created by Admin on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct AuenCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
